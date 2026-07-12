9bc214e891bb7076d873b83ed2288e5cd63d1413
Paolo Casagrande <paolocasagrande00@gmail.com>
2026-07-12 16:21:37 +0100

    positions comment added

--- parte1/Fabiano/Positions.cpp
+++ parte1/Fabiano/Positions.cpp
@@ -3,10 +3,16 @@
 std::map<Sect*, int> rigaDiSezione;
 std::map<Fld*, int> rigaDiField;
 
+/* Funzione registraRigaSezione: registra la riga di inizio di una sezione,
+usando il puntatore al nodo Sect* come chiave della mappa.
+*/
 void registraRigaSezione(Sect* s, int riga) {
     rigaDiSezione[s] = riga;
 }
 
+/* Funzione registraRigaField: registra la riga di inizio di un field,
+usando il puntatore al nodo Fld* come chiave della mappa.
+*/
 void registraRigaField(Fld* f, int riga) {
     rigaDiField[f] = riga;
 }
