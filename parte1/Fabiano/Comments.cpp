4813872577eb03757084bc421de2fffcbd0a0559
Paolo Casagrande <paolocasagrande00@gmail.com>
2026-07-12 16:22:17 +0100

    comments comment added

--- parte1/Fabiano/Comments.cpp
+++ parte1/Fabiano/Comments.cpp
@@ -2,6 +2,10 @@
 
 std::vector<CommentoTrovato> commentiTrovati;
 
+/* Funzione salvaCommento: chiamata dal lexer quando riconosce un commento,
+al posto di scartarlo (comportamento di default generato da BNFC) ne salva
+testo e riga per poterlo poi reinserire nel pretty-printer.
+*/
 void salvaCommento(const std::string& testo, int riga) {
     commentiTrovati.push_back({riga, testo});
 }
